3a3e70cfb0628217b84809c663c5393018278bd7
starkwang <381152119@qq.com>
2016-10-16 03:28:35 +0800

    code

--- frontend/src/page/IndexPage.js
+++ frontend/src/page/IndexPage.js
@@ -64,7 +64,7 @@ export default class IndexPage extends React.Component {
             })
         })
 
-        获取自身位置
+        // 获取自身位置
         navigator
             .geolocation
             .getCurrentPosition((result, err) => {
